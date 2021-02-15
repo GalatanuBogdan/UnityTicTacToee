b4afda0df80948e5a8a9094be62119f700c3fccc
Bogdan <galatanu.bogdan14@gmail.com>
2021-02-15 10:07:02 +0200

    Uploaded Correct Project

---
+++ UnityProject/Assets/ButtonController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite xImage;
+    public Sprite oImage;
+    private bool canBeChanged;
+    public int whatIsPutted; //1-x has been puted, 2 - 0 has been puted, 0 - not x or y has been puted
+    void Start()
+    {
+        canBeChanged = true;
+        whatIsPutted = 0;
+    }
+
+    public void AddXorY()
+    {
+        if (canBeChanged == true && GameObject.Find("GameManager").GetComponent<GameManager>().gameFinished == false)
+        {
+            bool whoMoves = GameObject.Find("GameManager").GetComponent<GameManager>().Player1Move;
+            if (whoMoves == true)//player 1 is moving
+            {
+                this.gameObject.GetComponent<Image>().sprite = xImage;
+                whatIsPutted = 1;
+
+                bool gameOver = GameObject.Find("GameManager").GetComponent<GameManager>().GameOver();
+
+                if(gameOver == true)
+                {
+                    GameObject.Find("GameOverStatus").GetComponent<Text>().text = "Player X has WON!";
+                    GameObject.Find("GameManager").GetComponent<GameManager>().gameFinished = true;
+                }
+                else
+                {
+                    bool nobodyCanWon = GameObject.Find("GameManager").GetComponent<GameManager>().NobodyCanWon();
+                
+                    if(nobodyCanWon == true)
+                    {
+                        GameObject.Find("GameManager").GetComponent<GameManager>().gameFinished = true;
+                        GameObject.Find("GameOverStatus").GetComponent<Text>().text = "Nobody has WON!";
+                    }
+
+                }
+
+                whoMoves = false;
+                
+            }
+            else
+            {
+                this.gameObject.GetComponent<Image>().sprite = oImage;
+                whatIsPutted = 2;
+
+                bool gameOver = GameObject.Find("GameManager").GetComponent<GameManager>().GameOver();
+
+                if (gameOver == true)
+                {
+                    GameObject.Find("GameOverStatus").GetComponent<Text>().text = "Player 0 has WON!";
+                    GameObject.Find("GameManager").GetComponent<GameManager>().gameFinished = true;
+                }
+
+                bool nobodyCanWon = GameObject.Find("GameManager").GetComponent<GameManager>().NobodyCanWon();
+
+                if (nobodyCanWon == true)
+                {
+                    GameObject.Find("GameManager").GetComponent<GameManager>().gameFinished = true;
+                    GameObject.Find("GameOverStatus").GetComponent<Text>().text = "Nobody has WON!";
+                }
+
+
+                whoMoves = true;
+               
+            }
+
+            canBeChanged = false;
+            GameObject.Find("GameManager").GetComponent<GameManager>().Player1Move = whoMoves;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityProject/Assets/GameManager.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[,] buttons = new GameObject[3, 3];
+    public GameObject button;
+    public RectTransform tableTransform;
+    public bool Player1Move;
+    public bool gameFinished;
+    void Start()
+    {
+        gameFinished = false;
+        Player1Move = true;
+        CreateTable();
+    }
+
+    void CreateTable()
+    {
+        GameObject createdObj;
+        Vector3 pozitie = new Vector3(50, 0, 0);
+
+        for (int i = 0; i < 3; i++)
+        {
+            createdObj = Instantiate(button, tableTransform.transform);
+            createdObj.GetComponent<RectTransform>().anchoredPosition = pozitie;
+            buttons[0, i] = createdObj;
+            pozitie.x += 100;
+        }
+        pozitie = new Vector3(50, -100, 0);
+        for (int i = 0; i < 3; i++)
+        {
+            createdObj = Instantiate(button, tableTransform.transform);
+            createdObj.GetComponent<RectTransform>().anchoredPosition = pozitie;
+            buttons[1, i] = createdObj;
+            pozitie.x += 100;
+        }
+
+        pozitie = new Vector3(50, -200, 0);
+        for (int i = 0; i < 3; i++)
+        {
+            createdObj = Instantiate(button, tableTransform.transform);
+            createdObj.GetComponent<RectTransform>().anchoredPosition = pozitie;
+            buttons[2, i] = createdObj;
+            pozitie.x += 100;
+        }
+    }
+
+    public bool GameOver()
+    {
+
+        //parcurgere pentru fiecare linie
+        for (int i = 0; i < 3; i++)
+        {
+            bool line_i_full_of_X = true;
+            bool line_i_full_of_O = true;
+            for (int j = 0; j < 3; j++)
+            {
+                int x_puted = buttons[i, j].GetComponent<ButtonController>().whatIsPutted;
+                if(x_puted == 0)
+                {
+                    line_i_full_of_X = false;
+                    line_i_full_of_O = false;
+                }
+
+                if(x_puted == 1)
+                    line_i_full_of_O = false;
+
+                if (x_puted == 2)
+                    line_i_full_of_X = false;
+            } 
+
+            if (line_i_full_of_O || line_i_full_of_X)
+                return true;
+        }
+
+        for(int i = 0; i < 3; i++)
+        {
+            bool column_i_full_of_X = true;
+            bool column_i_full_of_O = true;
+
+            for(int j = 0;j < 3; j++)
+            {
+                int x_puted = buttons[j, i].GetComponent<ButtonController>().whatIsPutted;
+
+                if (x_puted == 0)
+                {
+                    column_i_full_of_X = false;
+                    column_i_full_of_O = false;
+                }
+
+                if (x_puted == 1)
+                    column_i_full_of_O = false;
+
+                if (x_puted == 2)
+                    column_i_full_of_X = false;
+            }
+            if (column_i_full_of_O || column_i_full_of_X)
+                return true;
+        }
+        bool diagonal_full_of_X = true;
+        bool diagonal_full_of_O = true;
+        for (int i=0;i<3;i++)
+        {
+            int x_puted = buttons[i, i].GetComponent<ButtonController>().whatIsPutted;
+
+            if (x_puted == 0)
+            {
+                diagonal_full_of_X = false;
+                diagonal_full_of_O = false;
+            } 
+
+            if (x_puted == 1)
+                diagonal_full_of_O = false;
+
+            if (x_puted == 2)
+                diagonal_full_of_X = false;
+        }
+        
+        if (diagonal_full_of_O || diagonal_full_of_X)
+            return true;
+
+        diagonal_full_of_X = true;
+        diagonal_full_of_O = true;
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (i + j == 2)
+                {
+                    int x_puted = buttons[i, j].GetComponent<ButtonController>().whatIsPutted;
+
+                    if (x_puted == 0)
+                    {
+                        diagonal_full_of_X = false;
+                        diagonal_full_of_O = false;
+                    }
+
+                    if (x_puted == 1)
+                        diagonal_full_of_O = false;
+
+                    if (x_puted == 2)
+                        diagonal_full_of_X = false;
+                }
+
+        if (diagonal_full_of_O || diagonal_full_of_X)
+            return true;
+
+        
+        return false;
+    }
+
+    public bool NobodyCanWon()
+    {
+        
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (buttons[i, j].GetComponent<ButtonController>().whatIsPutted == 0)
+                    return false;
+       
+        return true;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+
+}
